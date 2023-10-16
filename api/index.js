9192c1420bdef0cdcce104a7901463a6b5df9abd
Hung Phu <nguyenhungphu777@gmail.com>
2023-10-16 14:28:21 -0500

    create update user API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -21,6 +22,8 @@ app.listen(3000, () => {
 );
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
