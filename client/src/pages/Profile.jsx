1f836662632fb5859f4496d223d51f347de8645e
Hung Phu <nguyenhungphu777@gmail.com>
2023-10-20 15:38:21 -0500

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -129,6 +129,22 @@ export default function Profile() {
       setShowListingsError(true);
     }
   }
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(error);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error);
+    }
+  }
  
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -173,7 +189,7 @@ export default function Profile() {
               <p>{listing.name}</p>
             </Link>
             <div className='flex flex-col item-center'>
-              <button className='text-red-700 uppercase'>Delete</button>
+              <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
               <button className='text-green-700 uppercase'>Edit</button>
             </div>
           </div>
