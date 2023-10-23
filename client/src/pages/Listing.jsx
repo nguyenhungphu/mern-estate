68015d119f33d31c2f81c46e97755638750e3e2d
Hung Phu <nguyenhungphu777@gmail.com>
2023-10-22 21:22:18 -0500

    complete listing ui and styling

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules'
 import 'swiper/css/bundle';
+import { FaBath, FaBed, FaChair, FaMapMarkedAlt, FaMapMarkerAlt, FaParking, FaShare } from 'react-icons/fa'; 
 
 export default function Listing() {
     const params = useParams();
@@ -42,7 +43,7 @@ export default function Listing() {
             {loading && <p className='text-center my-7 text-2x'>Loading...</p>}
             {error && <p className='text-center my-7 text-2x'>Something went wrong!</p>}
             {listing && !loading && !error && 
-                <>
+                <div>
                     <Swiper navigation>
                         {listing.imageUrls.map((url) => (
                             <SwiperSlide key={url}>
@@ -52,7 +53,52 @@ export default function Listing() {
                             </SwiperSlide>
                         ))}
                     </Swiper>
-                </>
+                    <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+                        <p className='text-2xl font-semibold'>
+                            {listing.name} - ${' '}
+                            {listing.offer
+                                ? listing.discountPrice.toLocaleString('en-US')
+                                : listing.regularPrice.toLocaleString('en-US')}
+                            {listing.type === 'rent' && ' / month'}
+                        </p>
+                        <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+                            <FaMapMarkerAlt className='text-green-700' />
+                            {listing.address}
+                        </p>
+                        <div className='flex gap-4'>
+                            <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                            </p>
+                            {listing.offer && (
+                                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                ${+listing.regularPrice - +listing.discountPrice} OFF
+                                </p>
+                            )}
+                        </div>
+                        <p className='text-slate-800'>
+                            <span className='font-semibold text-black'>Description - {' '}</span>
+                                {listing.description}
+                        </p>
+                        <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+                            <li className='flex items-center gap-1 whitespace-nowrap'>
+                                <FaBed className='text-lg'/>
+                                {listing.bedrooms > 1 ? `${listing.bedrooms} beds` : `${listing.bedrooms} bed`}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap'>
+                                <FaBath className='text-lg'/>
+                                {listing.bathrooms > 1 ? `${listing.bathrooms} baths` : `${listing.bathrooms} bath`}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap'>
+                                <FaParking className='text-lg'/>
+                                {listing.parking ? 'Parking Spot' : 'No Parking'}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap'>
+                                <FaChair className='text-lg'/>
+                                {listing.furnished ? 'Furnished' : 'Not Furnished'}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             }
         </main>
     )
