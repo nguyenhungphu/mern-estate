a2c8fb4f9f06152e3fdd1493df9745ff9142e2bb
Hung Phu <nguyenhungphu777@gmail.com>
2023-10-24 15:54:04 -0500

    add listing link sharing feature to listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -11,6 +11,7 @@ import {
   FaChair,
   FaMapMarkerAlt,
   FaParking,
+  FaShare
 } from "react-icons/fa";
 import Contact from "../components/Contact";
 
@@ -19,6 +20,7 @@ export default function Listing() {
   const { currentUser } = useSelector((state) => state.user);
   SwiperCore.use([Navigation]);
 
+  const [copied, setCopied] = useState(false);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -68,13 +70,30 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
           <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
-            <p className="text-2xl font-semibold">
-              {listing.name} - ${" "}
+            <p className='text-2xl font-semibold'>
+              {listing.name} - ${' '}
               {listing.offer
-                ? listing.discountPrice.toLocaleString("en-US")
-                : listing.regularPrice.toLocaleString("en-US")}
-              {listing.type === "rent" && " / month"}
+                ? listing.discountPrice.toLocaleString('en-US')
+                : listing.regularPrice.toLocaleString('en-US')}
+              {listing.type === 'rent' && ' / month'}
             </p>
             <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
               <FaMapMarkerAlt className="text-green-700" />
