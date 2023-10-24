c072b0d88585d574251ed18c0d3eb147580f3833
Hung Phu <nguyenhungphu777@gmail.com>
2023-10-24 15:31:30 -0500

    add conditional rendering for listing price

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,40 +2,32 @@ export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About Phu Estate
+        About Your Estate
       </h1>
       <p className="mb-4 text-slate-700">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
+        Welcome to Your Estate, your trusted partner in the world
+        of real estate. We're not just a company; we're a team of dedicated
+        professionals who understand that finding your ideal real estate is more
+        than just a transaction – it's about finding a place you can call home,
+        a place where dreams come to life, and investments flourish.
       </p>
       <p className="mb-4 text-slate-700">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
+        Our journey began with a passion for connecting people with their
+        perfect properties. We know that real estate isn't just about bricks and
+        mortar; it's about the memories you create and the life you build within
+        those walls. That's why we're committed to guiding you every step of the
+        way, making the process as smooth and enjoyable as possible.
       </p>
       <p className="mb-4 text-slate-700">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
+        At Your Estate , our mission is simple but profound: to
+        help you find your ideal real estate. Whether you're a first-time
+        homebuyer, an experienced investor, or someone looking for a new chapter
+        in your life, we are here to provide expertise, guidance, and unwavering
+        support. We are committed to ensuring that your real estate journey is
+        not just a transaction but a memorable and rewarding experience. Our
+        team of dedicated professionals is ready to go the extra mile to make
+        your real estate dreams a reality. Explore our website, reach out to us,
+        and let's embark on this exciting journey together.
       </p>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -56,7 +56,7 @@ export default function Home() {
           home with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Phu Estate is the best place to find your next perfect home to live.
+          Your Estate is the best place to find your next perfect home to live.
           <br />
           We have a wide range of properties for you to choose from.
         </div>
